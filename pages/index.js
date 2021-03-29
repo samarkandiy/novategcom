c85dca2eb2eb140d4ef20db82aa6f7acaaffbd12
Avaz Bokiev <html5web@gmail.com>
2021-03-28 21:22:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,11 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Novateg</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
           Coming soon!
         </p>
